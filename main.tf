32c48c625f9e1d217ec77dd4d106c52dc46cb100
chendoh <72640611+chendoh@users.noreply.github.com>
2025-04-22 19:28:59 -0500

    this is to create an s3 bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
 # this is for creating s3 buckets
+
+provider "aws" {
+  region = "us-east-1"  # Change to your desired region
+}
+
+resource "aws_s3_bucket" "immawalts_github" {
+  bucket = "immawalts-github"
+
+  tags = {
+    Name        = "immawalts-github"
+    Environment = "Dev"
+  }
+}
